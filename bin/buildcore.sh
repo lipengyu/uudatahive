4b0a84f7e1f7397bc112b0134187c66322763504
rock <xingang.kan@gmail.com>
2016-08-10 18:18:40 +0800

    init

--- bin/buildcore.sh
+++ bin/buildcore.sh
@@ -3,37 +3,32 @@
 #echo $UUMAI_HOME
 
 #UUMAI_HOME=/home/rock/kanxg/Dropbox/mysourcecode/uumai/bitbucket
-#UUMAI_JAVA_OPTS=" -Xms20m -Xmx100m " 
+#UUMAI_JAVA_OPTS=" -Xms20m -Xmx100m "
 
-echo "start to build core..." 
+echo "start to build core..."
 
 
-echo "clean old logs and libs..." 
+echo "clean old logs and libs..."
 
 rm -rf ./build/fengchao/libs/*
 rm -rf ./build/fengchao/logs/*
 
 
-echo "build  uumai-common..." 
+echo "build  uumai..."
 
-cd  ./fengchao/uumai-common
-rm  -rf ./common-libs/libs
-mvn clean install  
+cd  ./fengchao/
+rm  -rf ./uumai-common/common-libs/libs
+mvn clean install
 
-echo "build  uumai-core..." 
-cd  ../uumai-core
-mvn clean install 
-
-
-echo "build  install package..." 
-cd ../../
+echo "build  install package..."
+cd ..
 cp -r ./fengchao/uumai-common/common-libs/libs/* ./build/fengchao/libs/
 
 find ./fengchao/uumai-common  -name 'uumai-*.jar' | xargs -i{}  cp {}  ./build/fengchao/libs/
 find ./fengchao/uumai-core  -name 'uumai-*.jar' | xargs -i{}  cp {}  ./build/fengchao/libs/
 
 
-#echo "build uumai yarn" 
+#echo "build uumai yarn"
 
 #mvn package -f ./shop_indexer/uumai-yarn/pom.xml
 
@@ -47,13 +42,13 @@ echo "build apps..."
 
 ./bin/buildapp.sh quartz
 
-mvn package -f ./crawler-website/crawler-quartz/pom-storm.xml 
+mvn package -f ./crawler-website/crawler-quartz/pom-storm.xml
 
 ./bin/buildapp.sh chart2png
 ./bin/buildapp.sh quartz-client
 ./bin/buildapp.sh quartz-cloudinfotech
 
-echo "build finished!" 
+echo "build finished!"
 
 
 
